f1569d3714d26b0e21030af681f8e2701e0fe428
antonD24 <114072802+antonD24@users.noreply.github.com>
2022-10-18 17:47:22 +0100

    App

--- HSApp/HSApp/HSApp/Program.cs
+++ HSApp/HSApp/HSApp/Program.cs
@@ -16,7 +16,7 @@ namespace HSApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Addresses());
+            Application.Run(new MainMenu());
         }
     }
 }
